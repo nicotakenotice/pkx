8151035365db604f3ab7015e2f677c0df09b4d7d
Nico <nicola.zorzo@gmail.com>
2024-07-13 15:21:23 +0200

    add swiperjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from '@angular/core'
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit, signal } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { PokemonClient } from 'pokenode-ts'
 
@@ -11,6 +11,7 @@ interface Pokemon {
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -19,55 +20,55 @@ export class AppComponent implements OnInit {
   selectedPokemon = signal<Pokemon | null>(null)
 
   ngOnInit(): void {
+    const pokemonNames = [
+      'bulbasaur',
+      'ivysaur',
+      'venusaur',
+      'charmander',
+      'charmeleon',
+      'charizard',
+      'squirtle',
+      'wartortle',
+      'blastoise',
+      'pikachu'
+      // 'raichu',
+      // 'gastly',
+      // 'haunter',
+      // 'gengar',
+      // 'eevee',
+      // 'vaporeon',
+      // 'jolteon',
+      // 'flareon',
+      // 'snorlax',
+      // 'articuno',
+      // 'zapdos',
+      // 'moltres',
+      // 'dratini',
+      // 'dragonair',
+      // 'dragonite',
+      // 'mewtwo',
+      // 'mew'
+    ]
+
+    this.pokemons.set(
+      pokemonNames.map((name) => ({ name, sprite: `sprites-animated/${name}.gif` }))
+    )
+
     setTimeout(async () => {
       const api = new PokemonClient()
 
       try {
-        // const pokemons = await api.listPokemons(0, 151);
-        // console.log(pokemons);
-        // const first = await api.getPokemonByName(pokemons.results[0].name);
-        // console.log(first);
+        // const pokemons = await api.listPokemons(0, 151)
+        // console.log(pokemons)
+        // const first = await api.getPokemonByName(pokemons.results[5].name)
+        // console.log(first)
         // this.selectedPokemon.set({
         //   name: first.name,
         //   sprite: `sprites/${first.id}.png`
-        // });
+        // })
       } catch (err) {
-        console.error(err);
+        console.error(err)
       }
-
-      const pokemonNames = [
-        'bulbasaur',
-        'ivysaur',
-        'venusaur',
-        'charmander',
-        'charmeleon',
-        'charizard',
-        'squirtle',
-        'wartortle',
-        'blastoise',
-        'pikachu',
-        // 'raichu',
-        // 'gastly',
-        // 'haunter',
-        // 'gengar',
-        // 'eevee',
-        // 'vaporeon',
-        // 'jolteon',
-        // 'flareon',
-        // 'snorlax',
-        // 'articuno',
-        // 'zapdos',
-        // 'moltres',
-        // 'dratini',
-        // 'dragonair',
-        // 'dragonite',
-        // 'mewtwo',
-        // 'mew'
-      ]
-
-      this.pokemons.set(
-        pokemonNames.map((name) => ({ name, sprite: `sprites-animated/${name}.gif` }))
-      )
     }, 0)
   }
 }
