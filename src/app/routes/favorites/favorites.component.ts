32f233b09deeb4f42b8870268a077dd1fd759dc2
Nico <nicola.zorzo@gmail.com>
2024-08-09 15:50:51 +0200

    add list and grid view

--- src/app/routes/favorites/favorites.component.ts
+++ src/app/routes/favorites/favorites.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, inject, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { HeaderService, PokemonService } from '@lib/services';
 
@@ -14,9 +14,15 @@ export class FavoritesComponent implements OnInit {
   readonly headerService = inject(HeaderService);
   readonly pokemonService = inject(PokemonService);
 
+  displayMode = signal<'list' | 'grid'>('list');
   pokemons = computed(() => this.pokemonService.pokemons().filter((p) => p.isFavorite));
 
   ngOnInit(): void {
     this.headerService.title.set('Favorites');
+  }
+
+  toggleDisplayMode(): void {
+    const currentMode = this.displayMode();
+    this.displayMode.set(currentMode === 'list' ? 'grid' : 'list');
   }
 }
