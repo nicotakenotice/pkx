2ef1dc9d265d5ca3b1502bb71553f436622c7046
Nico <nicola.zorzo@gmail.com>
2024-08-13 23:33:17 +0200

    test hammerjs

--- src/app/routes/favorites/favorites.component.ts
+++ src/app/routes/favorites/favorites.component.ts
@@ -1,7 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  effect,
+  ElementRef,
+  inject,
+  OnInit,
+  signal,
+  viewChildren
+} from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { HeaderService, PokemonService } from '@lib/services';
+
+declare var Hammer: any;
 
 @Component({
   selector: 'app-favorites',
@@ -16,6 +27,30 @@ export class FavoritesComponent implements OnInit {
 
   displayMode = signal<'list' | 'grid'>('list');
   pokemons = computed(() => this.pokemonService.pokemons().filter((p) => p.isFavorite));
+  listItems = viewChildren<ElementRef<HTMLDivElement>>('listItem');
+
+  constructor() {
+    effect(() => {
+      const listItems = this.listItems().map((o) => o.nativeElement);
+      listItems.forEach((element) => {
+        const hammer = new Hammer(element);
+        hammer.on('panleft panright', (e: any) => {
+          // console.log(e);
+
+          if (e.isFirst) {
+            element.style.transitionDuration = '0s';
+          }
+
+          element.style.left = `${e.deltaX}px`;
+          
+          if (e.isFinal) {
+            element.style.left = '0px';
+            element.style.transitionDuration = '0.2s';
+          }
+        });
+      });
+    });
+  }
 
   ngOnInit(): void {
     this.headerService.title.set('Favorites');
