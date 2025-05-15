3502d148865c5d38150da12b089c0787eb9fa855
Nico <nicola.zorzo@gmail.com>
2025-05-15 22:36:54 +0200

    remove unused code

--- src/app/lib/services/index.ts
+++ src/app/lib/services/index.ts
@@ -1,6 +1,4 @@
 export * from './cool-things.service';
 export * from './header.service';
-export * from './log-update.service';
 export * from './pokemon.service';
 export * from './theme.service';
-
