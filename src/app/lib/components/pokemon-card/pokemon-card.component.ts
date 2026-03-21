2354ef3ebac631621563f2442b2c5a17684da8f2
Nico <nicola.zorzo@gmail.com>
2026-03-21 19:15:48 +0100

    refactor: remove dead ColorThief code from PokemonCardComponent

--- src/app/lib/components/pokemon-card/pokemon-card.component.ts
+++ src/app/lib/components/pokemon-card/pokemon-card.component.ts
@@ -1,15 +1,5 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  inject,
-  input,
-  output,
-  signal,
-  viewChild
-} from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { PokemonCard } from '@lib/models';
-import { CoolThingsService } from '@lib/services';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -17,31 +7,11 @@ import { CoolThingsService } from '@lib/services';
   templateUrl: './pokemon-card.component.html',
   styleUrl: './pokemon-card.component.css'
 })
-export class PokemonCardComponent implements AfterViewInit {
-  readonly coolThingsService = inject(CoolThingsService);
+export class PokemonCardComponent {
   readonly animations = ['jello', 'wobble', 'bounce', 'rotate-scale-up'];
 
   pokemon = input.required<PokemonCard>();
   onDetailsClick = output<string>();
-
-  divGradientRef = viewChild<ElementRef<HTMLDivElement>>('divGradientRef');
-  imgRef = viewChild<ElementRef<HTMLImageElement>>('imgRef');
-  imgMainColor = signal<string>('transparent');
-
-  ngAfterViewInit(): void {
-    // const colorThief = this.coolThingsService.colorThief;
-    // const divGradient = this.divGradientRef()?.nativeElement;
-    // const img = this.imgRef()?.nativeElement;
-    // setTimeout(async () => {
-    //   if (img && img.complete) {
-    //     const [r, g, b] = await colorThief.getColor(img);
-    //     this.imgMainColor.set(`rgb(${r}, ${g}, ${b})`);
-    //     divGradient?.classList.add('fade-in');
-    //   } else {
-    //     console.log(`${this.pokemon().name} image not loaded yet`);
-    //   }
-    // }, 200);
-  }
 
   startAnimation(e: Event): void {
     const randomAnimation = this.animations[Math.floor(Math.random() * this.animations.length)];
